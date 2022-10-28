b7da1ac93be6b6993f47be33c529c276b98cad5c
Marko Dimitrov <marecar9685@hotmail.com>
2022-10-28 22:30:50 +0200

    Add project files.

---
+++ IT-EComm/Models/Laptop.cs
@@ -0,0 +1,32 @@
+﻿namespace IT_EComm.Models
+{
+    public class Laptop
+    {
+
+        //General information
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        //Ram memory
+        public int RAMMemory { get; set; }
+
+        //Graphic Card
+        public string GraphicCardBrand { get; set; }
+        public string GraphicCardModel { get; set; }
+        public int GraphicCardMemory { get; set; }
+
+        //CPU
+        public string ProcessorBrand { get; set; }
+        public string ProcessorModel { get; set; }
+        public int ProcessorNoCores { get; set; }
+        public double CPUSpeed { get; set; }
+
+
+        //Storage
+
+
+        //Display and other info
+
+    }
+}
