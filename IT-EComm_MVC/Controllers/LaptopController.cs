24566584cfab85397a0674e10143685d325f5e67
Marko Dimitrov <marko.dimitrov96@gmail.com>
2022-11-07 22:59:26 +0100

    Added LaptopController and defined home pages for IT-ECom

---
+++ IT-EComm_MVC/Controllers/LaptopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT_EComm_MVC.Controllers
+{
+    public class LaptopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
